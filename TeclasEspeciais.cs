c0168c5fb0122c2ae8a1b13fe56c19e37b6736a7
Walter Dalla Torre Neto <wwddttnn@hotmail.com>
2018-10-01 00:04:24 -0300

    Add files via upload

---
+++ TeclasEspeciais.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Autores:
+            // Walter Dalla Torre Neto 17254 e Joao Henrique 17...
+
+            ConsoleKeyInfo b;
+            do
+            {
+
+                b = Console.ReadKey();
+
+                if (b.Key.GetHashCode() == 112)
+                    Console.WriteLine("Ajuda do sistema!");
+
+            } while ((b.Modifiers != ConsoleModifiers.Alt) || (b.KeyChar != 'x'));
+        }
+    }
+}
